703833a985b2072235d4d5c37c58b39b3ce8c8b8
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 17:48:33 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
   # title == "Home Alone"
   # release_date == 1990
 
-  Movie.create do |args|
+  Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
     m.save
