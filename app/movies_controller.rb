ae7389429574497438e4f3b343460571d3a8e9c8
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 17:48:04 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,8 +28,8 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
   # title == "Home Alone"
   # release_date == 1990
 
-  Movie.create do |m|
-    __
+  Movie.create do |args|
+
   end
 end
 
